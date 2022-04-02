19797ab99607cd787ad54e8b8409ac192d29cf9f
oguzhanv <oguzhan.varli2@hotmail.com>
2022-04-03 00:20:19 +0300

    insaat projesi bitimi
    Tüm projeler tamamlanmıştır.

---
+++ blog_sitesi_oguzhan_varli/script.js
@@ -0,0 +1,17 @@
+
+$(document).ready(function(){
+    
+    $(".navbar-ul").click(function(e){
+        $(".navbar-ul > a").removeClass("active")
+        $(e.target).addClass("active");
+
+    })
+    
+
+    
+    
+    
+    
+})
+
+
